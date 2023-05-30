391bc36ba59edad4b5f69cfadb56aab2e6ed70b2
RamsYang2023 <122635747+RamsYang2023@users.noreply.github.com>
2023-05-30 11:55:40 -0600

    Update build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Check the input is null or not
-if[[-z "$1" || -z "$2"]]
+if [ "$1" == '' ] || [ "$2" == '' ]
     then
        echo "Error: requirement for arguments is not satisfied"
        echo "Format: $0 <container_name> <image_name>
        exit 1
 fi
 
-Container_name = $1
-Image_name = $2
+Container_name=$1
+Image_name=$2
 
 # Run the CMD to creat and run a docker container
-docker run -d --name "$Container_name" "Image_name"
+docker run -it --name "$Container_name" "Image_name"
